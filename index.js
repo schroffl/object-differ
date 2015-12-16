3994db61a04e2e6bc7152324591814340c59aa87
Hannes <Hannes.Holzwarth@me.com>
2015-12-16 12:45:12 +0100

    Bugfix
    Fixed an issue where the value would not be updated when no callback was passed

--- index.js
+++ index.js
@@ -10,8 +10,8 @@ function differObject(curr, repl, callbackFunction, strict, path) {
 
                     if(typeof curr[key] === 'object') differObject(curr[key], repl[key], callbackFunction, strict, newPath);
                     else if(typeof curr[key] !== 'object') differObject((curr[key] = {}), repl[key], callbackFunction, strict, newPath);
-                } else if(typeof callbackFunction === 'function') {
-                    callbackFunction(key, repl[key], curr[key], newPath);
+                } else {
+                    if(typeof callbackFunction === 'function') callbackFunction(key, repl[key], curr[key], newPath);
                     curr[key] = repl[key];
                 }
             }
