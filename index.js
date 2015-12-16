aa9664901cabe0812893a4a0b3de4848fc7a4e20
Hannes <Hannes.Holzwarth@me.com>
2015-12-16 13:30:17 +0100

    You can now get notified about whole objects changing
    By supplying true as the 4th argument you will also be notified about objects changing

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-function differObject(curr, repl, callbackFunction, strict, path) {
+function differObject(curr, repl, callbackFunction, notifyObjects, path) {
     if(curr != repl) {
 		for(var key in repl) {
             if(curr[key] != repl[key]) {
@@ -8,8 +8,11 @@ function differObject(curr, repl, callbackFunction, strict, path) {
                 if(typeof repl[key] === 'object') {
                     newPath.push(key);
 
-                    if(typeof curr[key] === 'object') differObject(curr[key], repl[key], callbackFunction, strict, newPath);
-                    else if(typeof curr[key] !== 'object') differObject((curr[key] = {}), repl[key], callbackFunction, strict, newPath);
+                    if(typeof curr[key] === 'object') differObject(curr[key], repl[key], callbackFunction, notifyObjects, newPath);
+                    else if(typeof curr[key] !== 'object') {
+                        if(notifyObjects) callbackFunction(key, repl[key], curr[key], newPath);
+                        differObject((curr[key] = {}), repl[key], callbackFunction, notifyObjects, newPath);
+                    }
                 } else {
                     if(typeof callbackFunction === 'function') callbackFunction(key, repl[key], curr[key], newPath);
                     curr[key] = repl[key];
