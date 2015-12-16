44d9baf33ad2eb252dd35f8939780f7d3a97144b
Hannes <Hannes.Holzwarth@me.com>
2015-12-16 12:44:27 +0100

    Added benchmark (test/benchmark.js)

---
+++ test/benchmark.js
@@ -0,0 +1,26 @@
+var codeStart = process.hrtime();
+var differ = require('../index.js');
+
+var obj = {
+    'This': 'is',
+    'a': {
+        'test': 'object',
+        'demonstrating': 'the'
+    },
+    'functionality': ['of', 'object-differ']
+};
+
+var update = {
+    'a': {
+        'test': 'that isn\'t'
+    }
+};
+
+var updateStart = process.hrtime();
+differ(obj, update);
+
+var updateDiff = process.hrtime(updateStart);
+var codeDiff = process.hrtime(codeStart);
+
+console.log('Full execution took %dms', codeDiff[0] + (codeDiff[1] / 1000000));
+console.log('Bare updating took %dms', updateDiff[0] + (updateDiff[1] / 1000000));
