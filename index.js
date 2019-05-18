5fcf7606dd366ad81bb8b44d527605af371fc5c4
schroffl <schroffl@users.noreply.github.com>
2019-05-18 11:24:40 +0200

    Remove legacy code/comment

--- index.js
+++ index.js
@@ -1,48 +1,5 @@
 'use strict';
 
-/*function differObject(curr, repl, callbackFunction, notifyObjects, strict, path) {
-    if(curr != repl) {
-        if(!Array.isArray(path)) path = [];
-        var newPath = path.slice();
-
-		for(var key in repl) {
-            if(curr[key] != repl[key]) {
-
-                if(typeof repl[key] === 'object') {
-                    newPath.push(key);
-
-                    if(typeof curr[key] === 'object') {
-                        if(notifyObjects && typeof callbackFunction === 'function') callbackFunction(key, repl[key], curr[key], path);
-                        differObject(curr[key], repl[key], callbackFunction, notifyObjects, strict, newPath);
-                    } else if(typeof curr[key] !== 'object') {
-                        if(notifyObjects && typeof callbackFunction === 'function') callbackFunction(key, repl[key], curr[key], path);
-                        differObject((curr[key] = {}), repl[key], callbackFunction, notifyObjects, strict, newPath);
-                    }
-                } else {
-                    if(typeof callbackFunction === 'function') callbackFunction(key, repl[key], curr[key], newPath);
-                    curr[key] = repl[key];
-                }
-            }
-		}
-
-        if(strict) {
-            for(var key in curr) {
-                if(!repl.hasOwnProperty(key)) {
-                    if(typeof curr[key] === 'object') {
-                        newPath.push(key);
-
-                        if(notifyObjects && typeof callbackFunction === 'function') callbackFunction(key, undefined, curr[key], path);
-                        differObject(curr[key], {}, callbackFunction, notifyObjects, strict, newPath);
-                    }
-
-                    if(typeof callbackFunction === 'function') callbackFunction(key, undefined, curr[key], newPath);
-                    delete curr[key];
-                }
-            }
-        }
-    }
-}*/
-
 function propagateChange(callback, key, previous, current, path) {
     if(typeof callback === 'function') callback(key, previous, current, path);
 }
